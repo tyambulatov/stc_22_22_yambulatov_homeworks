e460b81ea729a23485b392a043da816d3a074677
Timur Yambulatov <t.yambulatov@gmail.com>
2022-12-01 18:16:38 +0300

    Updated HomeHork_12

--- HomeWork_12/src/main/sql/data.sql
+++ HomeWork_12/src/main/sql/data.sql
@@ -1,21 +1,21 @@
-insert into user_table (first_name, last_name, phone_number, driving_experience, age, got_driving_licence, driving_licence_category, rating)
-values ('Jean-Paul', 'Marat', '+337942768899', 4, 50, true, 'B', 3),
+insert into account (first_name, last_name, phone_number, driving_experience, age, has_license, driving_licence_category, rating)
+values ('Jean-Paul', 'Marat', '+337942768899', 4, 50, true, 'B', 3.3),
        ('Maximilien', 'Robespierre', '+339982289901', 10, 36, true, 'A', 5),
-       ('Louis', 'Saint-Just', '+334449092990', 11, 27, true, 'C', 2),
+       ('Louis', 'Saint-Just', '+334449092990', 11, 27, true, 'C', 2.1),
        ('Georges', 'Danton', '+339912229922', 3, 45, true, 'C', 0);
 ;
 
-insert into user_table (first_name, last_name, phone_number, age, got_driving_licence)
+insert into account (first_name, last_name, phone_number, age, has_license)
 values ('Louis', 'Capet', '+339920099231', 38, false);
 
-insert into car (model, colour, license_plate, driver_id)
+insert into car (model, color, license_plate, owner_id)
 values ('Adder', 'Black', 'AA-229-DB', 5),
        ('Brawler', 'Ivory', 'CA-221-AL', 4),
        ('Comet', 'Green', 'KL-429-LA', 4),
        ('Daemon', 'Navy', 'LK-555-LL', 1),
        ('Picador', 'Blue', 'LA-525-AL', 2);
 
-insert into ride (driver_id, car_id, ride_date, duration)
+insert into ride (owner_id, car_id, ride_date, duration)
 values (1, 1, '1791-01-02', '04:04:01'),
        (2, 5, '1931-01-14', '15:09:01'),
        (3, 5, '1945-11-10', '09:09:01'),
--- HomeWork_12/src/main/sql/schema.sql
+++ HomeWork_12/src/main/sql/schema.sql
@@ -1,33 +1,33 @@
 drop table if exists ride;
 drop table if exists car;
-drop table if exists user_table;
+drop table if exists account;
 
-create table user_table (
+create table account (
     id bigserial primary key,
     first_name varchar(20) not null ,
     last_name varchar(20) not null ,
     phone_number varchar(20) not null ,
-    driving_experience integer check ( driving_experience >= 0 and driving_experience <= 120 ),
-    age integer check ( age >= 0 and age <= 120 ) not null,
-    got_driving_licence bool,
+    driving_experience integer,
+    age integer not null,
+    has_license bool,
     driving_licence_category varchar(20),
-    rating integer check ( rating >= 0 and rating <= 5 )
+    rating real check ( rating >= 0 and rating <= 5 )
 );
 
 create table car (
     id bigserial primary key,
     model varchar(20),
-    colour varchar(20),
+    color varchar(20),
     license_plate varchar(20),
-    driver_id bigint not null,
-    foreign key (driver_id) references user_table(id)
+    owner_id bigint not null,
+    foreign key (owner_id) references account(id)
 );
 
 create table ride (
-    driver_id bigserial not null ,
+    owner_id bigserial not null ,
     car_id bigserial not null,
     ride_date date not null ,
     duration interval not null,
-    foreign key (driver_id) references user_table(id),
+    foreign key (owner_id) references account(id),
     foreign key (car_id) references car(id)
 );
